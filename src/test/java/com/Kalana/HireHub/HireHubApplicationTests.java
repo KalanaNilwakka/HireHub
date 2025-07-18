7fbebb632cede8bb51b0f2cd3c77f89ff3672c70
KalanaNilwakka <kalanad0507@gmail.com>
2025-07-18 13:31:33 +0530

    created initial structure

---
+++ src/test/java/com/Kalana/HireHub/HireHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Kalana.HireHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
