286982c006e4d2f914f6536a1879237c4b1dda84
Kalana Nilwakka <kalanad0507@gmail.com>
2025-09-03 08:27:29 +0530

    Implemented getUserById in user service

--- src/main/java/com/Kalana/HireHub/service/UserService.java
+++ src/main/java/com/Kalana/HireHub/service/UserService.java
@@ -1,9 +1,12 @@
 package com.Kalana.HireHub.service;
 
 import com.Kalana.HireHub.dto.UserDTO;
+
+import java.util.Optional;
 import java.util.Set;
 
 public interface UserService {
     UserDTO createUser(UserDTO userDTO);
     Set<UserDTO> getUsers();
+    UserDTO getUserById(Long id);
 }
--- src/main/java/com/Kalana/HireHub/service/impl/UserServiceImpl.java
+++ src/main/java/com/Kalana/HireHub/service/impl/UserServiceImpl.java
@@ -1,12 +1,15 @@
 package com.Kalana.HireHub.service.impl;
 
 import com.Kalana.HireHub.dto.UserDTO;
+import com.Kalana.HireHub.exception.UserNotFoundException;
 import com.Kalana.HireHub.model.User;
 import com.Kalana.HireHub.repository.UserRepository;
 import com.Kalana.HireHub.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -33,5 +36,12 @@ public class UserServiceImpl implements UserService {
                 user -> modelMapper.map(user, UserDTO.class))
                 .collect(Collectors.toSet()
         );
+    }
+
+    public UserDTO getUserById(Long id) {
+        if (userRepository.findById(id).isPresent())
+                return modelMapper.map(userRepository.findById(id).get(),UserDTO.class);
+        else
+            throw new UserNotFoundException(id);
     }
 }
