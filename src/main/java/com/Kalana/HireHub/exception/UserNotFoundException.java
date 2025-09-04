3fa6115d90e47c9edb32f7ec407c8a096fcef92b
Kalana Nilwakka <kalanad0507@gmail.com>
2025-09-05 01:48:52 +0530

    Implemented new exception for email

--- src/main/java/com/Kalana/HireHub/exception/UserNotFoundException.java
+++ src/main/java/com/Kalana/HireHub/exception/UserNotFoundException.java
@@ -4,4 +4,8 @@ public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(Long userId) {
         super("User with " +  userId + " not found");
     }
+
+    public UserNotFoundException(String email) {
+        super("User with " +  email + " not found");
+    }
 }
