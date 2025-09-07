34be78f291bc3f8716cf4285bafeaa9406869236
Kalana Nilwakka <kalanad0507@gmail.com>
2025-09-07 18:49:23 +0530

    Created endpoint for createJob

--- src/main/java/com/Kalana/HireHub/controller/JobController.java
+++ src/main/java/com/Kalana/HireHub/controller/JobController.java
@@ -1,7 +1,12 @@
 package com.Kalana.HireHub.controller;
 
+import com.Kalana.HireHub.dto.CRUDRepositoryDTO;
+import com.Kalana.HireHub.dto.JobDTO;
 import com.Kalana.HireHub.service.JobService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +18,12 @@ public class JobController {
 
     public JobController(JobService jobService) {
         this.jobService = jobService;
+    }
+
+    @PostMapping
+    public ResponseEntity<CRUDRepositoryDTO<JobDTO>> createJob(@RequestBody JobDTO jobDTO) {
+        return ResponseEntity.ok(new CRUDRepositoryDTO<>(
+                true, "Job created successfully",  jobService.createJob(jobDTO)
+        ));
     }
 }
