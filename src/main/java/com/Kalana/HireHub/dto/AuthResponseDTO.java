c0973935a4e5ca51e51d8f55974f371d9218d1f9
Kalana Nilwakka <kalanad0507@gmail.com>
2025-11-30 19:09:27 +0530

    Created AuthResponseDTO for sending JWT

---
+++ src/main/java/com/Kalana/HireHub/dto/AuthResponseDTO.java
@@ -0,0 +1,12 @@
+package com.Kalana.HireHub.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class AuthResponseDTO {
+    private String token;
+}
