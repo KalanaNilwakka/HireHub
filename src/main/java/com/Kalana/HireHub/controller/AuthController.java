7175e1dbec230e859f6e3a88903f191fbd88a38c
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-07 23:32:37 +0530

    Update

--- src/main/java/com/Kalana/HireHub/controller/AuthController.java
+++ src/main/java/com/Kalana/HireHub/controller/AuthController.java
@@ -3,15 +3,14 @@ package com.Kalana.HireHub.controller;
 import com.Kalana.HireHub.dto.AuthRequestDTO;
 import com.Kalana.HireHub.dto.AuthResponseDTO;
 import com.Kalana.HireHub.security.jwt.JwtUtil;
+import com.Kalana.HireHub.security.user.CustomUserDetails;
 import com.Kalana.HireHub.security.user.CustomUserDetailsService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -33,11 +32,13 @@ public class AuthController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody AuthRequestDTO authRequestDTO) {
-        authenticationManager.authenticate(
+        Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(authRequestDTO.getUsername(), authRequestDTO.getPassword())
         );
 
-        final UserDetails userDetails = customUserDetailsService.loadUserByUsername(authRequestDTO.getUsername());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        final CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
         final String token = jwtUtil.generateToken(userDetails);
 
         return ResponseEntity.ok(new AuthResponseDTO(token));
