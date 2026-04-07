bb3112e0dc4b43d397426e577d6ef5c3e62b3db9
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-07 23:40:52 +0530

    Created RoleRepository file

---
+++ src/main/java/com/Kalana/HireHub/repository/RoleRepository.java
@@ -0,0 +1,7 @@
+package com.Kalana.HireHub.repository;
+
+import com.Kalana.HireHub.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role,Long> {
+}
