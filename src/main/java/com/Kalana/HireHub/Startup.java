f9deb031d9e7e740166310c13c452bc205f13d64
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-07 23:38:42 +0530

    Created Startup.java file

---
+++ src/main/java/com/Kalana/HireHub/Startup.java
@@ -0,0 +1,11 @@
+package com.Kalana.HireHub;
+
+import org.springframework.boot.CommandLineRunner;
+
+
+public class Startup implements CommandLineRunner {
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
