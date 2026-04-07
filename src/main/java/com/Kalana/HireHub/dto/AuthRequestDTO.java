a2bc9916b11ad91a805871adf9fbed064b2798ef
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-07 23:32:57 +0530

    Update

--- src/main/java/com/Kalana/HireHub/dto/AuthRequestDTO.java
+++ src/main/java/com/Kalana/HireHub/dto/AuthRequestDTO.java
@@ -1,5 +1,7 @@
 package com.Kalana.HireHub.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,11 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class AuthRequestDTO {
+
+    @NotNull
+    @Email
     private String username;
+
+    @NotNull
     private String password;
 }
