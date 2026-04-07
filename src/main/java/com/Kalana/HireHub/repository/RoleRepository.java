521922192c15411b3d399db268c21f5ba84b045f
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-07 23:50:35 +0530

    Implemented method to find Roles by role name

--- src/main/java/com/Kalana/HireHub/repository/RoleRepository.java
+++ src/main/java/com/Kalana/HireHub/repository/RoleRepository.java
@@ -3,5 +3,8 @@ package com.Kalana.HireHub.repository;
 import com.Kalana.HireHub.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role,Long> {
+    Optional<Role> findByName(String name);
 }
