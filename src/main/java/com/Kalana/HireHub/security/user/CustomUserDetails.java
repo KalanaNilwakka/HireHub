88d66da14d95d7866059b60adb3940d484068e98
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-07 23:33:43 +0530

    Update

--- src/main/java/com/Kalana/HireHub/security/user/CustomUserDetails.java
+++ src/main/java/com/Kalana/HireHub/security/user/CustomUserDetails.java
@@ -1,34 +1,54 @@
 package com.Kalana.HireHub.security.user;
 
 import com.Kalana.HireHub.model.User;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@AllArgsConstructor
 public class CustomUserDetails implements UserDetails {
-    private final User user;
 
-    public CustomUserDetails(User user) {
-        this.user = user;
+    private final Long userId;
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    @JsonIgnore
+    private final String password;
+    private final Collection<? extends GrantedAuthority> authorities;
+
+    public static CustomUserDetails build(User user){
+        List<GrantedAuthority>authorities = user.getUserRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getUserRole().name()))
+                .collect(Collectors.toList());
+        return new CustomUserDetails(
+                user.getUserId(),
+                user.getFirstName(),
+                user.getLastName(),
+                user.getEmail(),
+                user.getPassword(),
+                authorities
+        );
     }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return user.getUserRoles().stream()
-                .map(role -> new SimpleGrantedAuthority(role.getUserRole().name()))
-                .toList();
+        return authorities;
     }
 
     @Override
     public String getPassword() {
-        return user.getPassword();
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return user.getEmail();
+        return email;
     }
 
     @Override
