77bdd9d44ef760af3bc69a6ca5298290a4d9649e
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-09 12:28:59 +0530

    Update

--- src/main/java/com/Kalana/HireHub/repository/RoleRepository.java
+++ src/main/java/com/Kalana/HireHub/repository/RoleRepository.java
@@ -1,10 +1,11 @@
 package com.Kalana.HireHub.repository;
 
 import com.Kalana.HireHub.model.Role;
+import com.Kalana.HireHub.model.enums.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Role,Long> {
-    Optional<Role> findByName(String name);
+    Optional<Role> findByName(UserRole name);
 }
