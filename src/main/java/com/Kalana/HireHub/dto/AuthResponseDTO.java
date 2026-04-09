5672363e73bf3415c28678e701f971e81066faf2
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-09 12:27:03 +0530

    Updated to include authorities

--- src/main/java/com/Kalana/HireHub/dto/AuthResponseDTO.java
+++ src/main/java/com/Kalana/HireHub/dto/AuthResponseDTO.java
@@ -4,9 +4,12 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class AuthResponseDTO {
     private String token;
+    private List<String> userRoles;
 }
