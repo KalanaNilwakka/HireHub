78a98eecaec47f3de5d3ea49c112e572afbfa59a
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-15 01:14:49 +0530

    Implemented method to get an application using User and Job

--- src/main/java/com/Kalana/HireHub/repository/ApplicationRepository.java
+++ src/main/java/com/Kalana/HireHub/repository/ApplicationRepository.java
@@ -1,9 +1,12 @@
 package com.Kalana.HireHub.repository;
 
 import com.Kalana.HireHub.model.Application;
+import com.Kalana.HireHub.model.Job;
+import com.Kalana.HireHub.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
+    Boolean existsByUserAndJob(User user, Job job);
 }
