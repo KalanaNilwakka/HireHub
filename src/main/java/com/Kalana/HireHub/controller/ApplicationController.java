b7d6d1982b0a7922e072a7a5f4f66ad38a918ddc
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-15 01:16:17 +0530

    Implemented mendpoint to apply for the jobs

--- src/main/java/com/Kalana/HireHub/controller/ApplicationController.java
+++ src/main/java/com/Kalana/HireHub/controller/ApplicationController.java
@@ -1,8 +1,14 @@
 package com.Kalana.HireHub.controller;
 
+import com.Kalana.HireHub.dto.ApplicationDTO;
+import com.Kalana.HireHub.dto.CRUDRepositoryDTO;
 import com.Kalana.HireHub.service.ApplicationService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/applications")
@@ -10,5 +16,15 @@ public class ApplicationController {
 
     private final ApplicationService applicationService;
 
-    public ApplicationController(ApplicationService applicationService) {this.applicationService = applicationService;}
+    public ApplicationController(ApplicationService applicationService) {
+        this.applicationService = applicationService;
+    }
+
+    @PostMapping("/apply/{jobId}")
+    public ResponseEntity<CRUDRepositoryDTO<ApplicationDTO>> apply(
+            @RequestBody MultipartFile resume, @PathVariable Long jobId) throws IOException {
+        return new ResponseEntity<>(new CRUDRepositoryDTO<>(
+                true,"Applied for the job successfully",applicationService.apply(jobId,resume)
+        ), HttpStatus.CREATED);
+    }
 }
