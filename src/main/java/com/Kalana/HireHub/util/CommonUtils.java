8395874d9a6a690c80ae189ffa17f39b6582a17e
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-14 12:26:20 +0530

    Implemented method to get the logged in user details

--- src/main/java/com/Kalana/HireHub/util/CommonUtils.java
+++ src/main/java/com/Kalana/HireHub/util/CommonUtils.java
@@ -1,7 +1,18 @@
 package com.Kalana.HireHub.util;
 
+import com.Kalana.HireHub.security.user.CustomUserDetails;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CommonUtils {
+
+    public CustomUserDetails getLoggedInUser(){
+        SecurityContext context = SecurityContextHolder.getContext();
+        Authentication authentication = context.getAuthentication();
+        CustomUserDetails loggedInUser = (CustomUserDetails) authentication.getPrincipal();
+        return loggedInUser;
+    }
 }
