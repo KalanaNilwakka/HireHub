da593c95c34f797cf29eed6348b1f1228c02dfc5
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-16 23:02:06 +0530

    Implemented method to get the applications according to Job ID from the DB

--- src/main/java/com/Kalana/HireHub/repository/ApplicationRepository.java
+++ src/main/java/com/Kalana/HireHub/repository/ApplicationRepository.java
@@ -6,7 +6,10 @@ import com.Kalana.HireHub.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Set;
+
 @Repository
 public interface ApplicationRepository extends JpaRepository<Application, Long> {
     Boolean existsByUserAndJob(User user, Job job);
+    Set<Application> findByJob_JobId(Long jobId);
 }
