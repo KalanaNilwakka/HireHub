12ce5a86d9c00a0a9c12a22db1199e681c2e2b00
Kalana Nilwakka <kalanad0507@gmail.com>
2026-04-30 01:14:04 +0530

    Updated to match the frontend

--- src/main/java/com/Kalana/HireHub/config/CorsConfiguration.java
+++ src/main/java/com/Kalana/HireHub/config/CorsConfiguration.java
@@ -13,7 +13,10 @@ public class CorsConfiguration {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+                registry.addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
             }
         };
     }
